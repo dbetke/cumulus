c3660772ddb58d9cc547fc5b0d2c3d630942183f
dbetke <danielle.betke@gmail.com>
2013-07-16 22:51:43 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cumulus::Application.config.secret_token = 'e8c9b8ebffffad0b82b2d6fd2b6bfc1d4b0706e6659db7224c7d112b4f64c9ad52ad26eff26d5c563b543e003f201fffd6c2ae940fff328ac395fb8acb8fb183'
