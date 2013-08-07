5088d6f1536383fe5c598d1390d2e6efdcb4ddb9
Jay Sanders <mindtonic@gmail.com>
2013-08-07 09:12:21 -0400

    Removing unused methods

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -31,7 +31,6 @@ class CardsController < ApplicationController
 
   def update
 		@current_user.update_attributes(params[:user])
-    #Skill.document_user_info(params[:name], params[:box1], params[:box2], current_user)
 
  		respond_to do |format|
       format.html { redirect_to card_path }
